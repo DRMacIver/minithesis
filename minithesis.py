b77d3bdb3029d7b998dddfcfaa7dd10250377727
David R. MacIver <david@drmaciver.com>
2020-07-18 15:28:46 +0100

    Reorder code and add some docstrings

--- minithesis.py
+++ minithesis.py
@@ -18,10 +18,93 @@ from random import Random
 import dbm
 
 
+def run_test(max_examples=100, seed=None, database=None):
+    """Decorator to run a test. Usage is:
+
+    .. code-block: python
+
+        @run_test()
+        def _(test_case):
+            n = test_case.choice(1000)
+            ...
+
+    The decorated function takes a ``TestCase`` argument,
+    and should raise an exception to indicate a test failure.
+    It will either run silently or print drawn values and then
+    fail with an exception if minithesis finds some test case
+    that fails.
+
+    The test will be run immediately, unlike in Hypothesis where
+    @given wraps a function to expose it to the the test runner.
+    If you don't want it to be run immediately wrap it inside a
+    test function yourself.
+
+    Arguments:
+
+    * max_examples: the maximum number of valid test cases to run for.
+      Note that under some circumstances the test may run fewer test
+      cases than this.
+    * seed: A fixed seed to use for randomness.
+    * dict: A dict-like object in which results will be cached and resumed
+      from, ensuring that if a test is run twice it fails in the same way.
+    """
+
+    def accept(test):
+        random = Random(seed)
+
+        def mark_failures_interesting(test_case):
+            try:
+                test(test_case)
+            except Exception:
+                if test_case.status is not None:
+                    raise
+                test_case.mark_status(Status.INTERESTING)
+
+        state = TestingState(random, mark_failures_interesting, max_examples)
+
+        if database is None:
+            # We're using the DBM module because it's an easy default.
+            # We don't use this in real Hypothesis - we've got a weird
+            # thing there designed to be checked into git but honestly
+            # nobody ever checks it into git - and I would encourage you
+            # to use some more sensible key/value store here.
+            db = dbm.open(".minithesis-cache", "c")
+        else:
+            db = database
+
+        previous_failure = db.get(test.__name__)
+
+        if previous_failure is not None:
+            choices = [
+                int.from_bytes(previous_failure[i : i + 8], "big")
+                for i in range(0, len(previous_failure), 8)
+            ]
+            state.test_function(TestCase.for_choices(choices))
+
+        if state.result is None:
+            state.run()
+
+        if state.result is None:
+            db.pop(test.__name__, None)
+        else:
+            db[test.__name__] = b"".join(i.to_bytes(8, "big") for i in state.result)
+
+        if hasattr(db, "close"):
+            db.close()
+
+        if state.result is not None:
+            test(TestCase.for_choices(state.result, print_results=True))
+
+    return accept
+
+
 class Possibility(object):
-    """Represents something that can be generated.
+    """Represents some range of values that might be used in
+    a test, that can be requested from a ``TestCase``.
+
     Pass one of these to TestCase.any to get a concrete value.
     """
+
     def __init__(self, produce=None):
         if produce is not None:
             self.produce = produce
@@ -39,42 +122,25 @@ class Possibility(object):
         raise NotImplementedError()
 
 
-class Frozen(Exception):
-    """Attempted to make choices on a test case that has been
-    completed."""
-
-
-class StopTest(Exception):
-    """Raised when a test should stop executing early."""
-
-
-class Status(IntEnum):
-    # Test case didn't have enough data to complete
-    OVERRUN = 0
-
-    # Test case contained something that prevented completion
-    INVALID = 1
-
-    # Test case completed just fine but was boring
-    VALID = 2
-
-    # Test case completed and was interesting
-    INTERESTING = 3
-
-
 class TestCase(object):
     """Represents a single generated test case, which consists
     of an underlying set of choices that produce possibilities."""
+
     @classmethod
     def for_choices(cls, choices, print_results=False):
         """Returns a test case that makes this series of choices."""
-        return TestCase(prefix=choices, random=None, max_size=len(choices), print_results=print_results)
+        return TestCase(
+            prefix=choices,
+            random=None,
+            max_size=len(choices),
+            print_results=print_results,
+        )
 
-    def __init__(self, prefix, random, max_size=float('inf'), print_results=False):
+    def __init__(self, prefix, random, max_size=float("inf"), print_results=False):
         self.prefix = prefix
         self.random = random
         self.max_size = max_size
-        self.choices = array('I')
+        self.choices = array("I")
         self.status = None
         self.print_results = print_results
         self.depth = 0
@@ -124,7 +190,7 @@ class TestCase(object):
 
     def __should_print(self):
         return self.print_results and self.depth == 0
-    
+
     def __make_choice(self, n, rnd_method):
         """Make a choice in [0, n], by calling rnd_method if
         randomness is needed."""
@@ -144,10 +210,9 @@ class TestCase(object):
         return result
 
 
-
 # We cap the maximum amount of entropy a test case can use.
 # This prevents cases where the generated test case size explodes
-# by effectively rejection 
+# by effectively rejection
 BUFFER_SIZE = 8 * 1024
 
 
@@ -177,7 +242,9 @@ class TestingState(object):
         if test_case.status >= Status.VALID:
             self.valid_test_cases += 1
         if test_case.status == Status.INTERESTING:
-            if self.result is None or sort_key(test_case.choices) < sort_key(self.result):
+            if self.result is None or sort_key(test_case.choices) < sort_key(
+                self.result
+            ):
                 self.result = test_case.choices
 
     def run(self):
@@ -186,13 +253,13 @@ class TestingState(object):
 
     def generate(self):
         while (
-            self.result is None and
-            self.valid_test_cases < self.max_examples and
-            self.calls < self.max_examples * 10
+            self.result is None
+            and self.valid_test_cases < self.max_examples
+            and self.calls < self.max_examples * 10
         ):
-            self.test_function(TestCase(
-                prefix=(), random=self.random, max_size=BUFFER_SIZE
-            ))
+            self.test_function(
+                TestCase(prefix=(), random=self.random, max_size=BUFFER_SIZE)
+            )
 
     def shrink(self):
         if self.result is None:
@@ -225,7 +292,7 @@ class TestingState(object):
             while k > 0:
                 i = len(self.result) - k - 1
                 while i >= 0:
-                    attempt = self.result[:i] + self.result[i + k:]
+                    attempt = self.result[:i] + self.result[i + k :]
                     assert len(attempt) < len(self.result)
                     if not consider(attempt):
                         i -= 1
@@ -236,7 +303,9 @@ class TestingState(object):
             while k > 0:
                 i = len(self.result) - k - 1
                 while i >= 0:
-                    attempt = self.result[:i] + array('I', [0] * k) + self.result[i + k:]
+                    attempt = (
+                        self.result[:i] + array("I", [0] * k) + self.result[i + k :]
+                    )
                     if consider(attempt):
                         i -= k
                     else:
@@ -254,7 +323,7 @@ class TestingState(object):
                 hi = self.result[i]
                 while lo + 1 < hi:
                     mid = lo + (hi - lo) // 2
-                    attempt = array('I', self.result)
+                    attempt = array("I", self.result)
                     attempt[i] = mid
                     if consider(attempt):
                         hi = mid
@@ -263,60 +332,24 @@ class TestingState(object):
                 i -= 1
 
 
-def run_test(max_examples=100, seed=None, database=None):
-    """Decorator to run a test.
+class Frozen(Exception):
+    """Attempted to make choices on a test case that has been
+    completed."""
 
-    Arguments:
 
-    * max_examples: the maximum number of valid test cases to run for.
-      Note that under some circumstances the test may run fewer test
-      cases than this.
-    * seed: A fixed seed to use for randomness.
-    * dict: A dict-like object in which results will be cached and resumed
-      from, ensuring that if a test is run twice it fails in the same way.
-    """
-    def accept(test):
-        random = Random(seed)
+class StopTest(Exception):
+    """Raised when a test should stop executing early."""
 
-        def mark_failures_interesting(test_case):
-            try:
-                test(test_case)
-            except Exception:
-                if test_case.status is not None:
-                    raise
-                test_case.mark_status(Status.INTERESTING)
 
-        state = TestingState(
-            random, mark_failures_interesting, max_examples
-        )
+class Status(IntEnum):
+    # Test case didn't have enough data to complete
+    OVERRUN = 0
 
-        if database is None:
-            # We're using the DBM module because it's an easy default.
-            # We don't use this in real Hypothesis - we've got a weird
-            # thing there designed to be checked into git but honestly
-            # nobody ever checks it into git - and I would encourage you
-            # to use some more sensible key/value store here.
-            db = dbm.open('.minithesis-cache', 'c')
-        else:
-            db = database
+    # Test case contained something that prevented completion
+    INVALID = 1
 
-        previous_failure = db.get(test.__name__)
+    # Test case completed just fine but was boring
+    VALID = 2
 
-        if previous_failure is not None:
-            choices = [int.from_bytes(previous_failure[i:i+8], 'big') for i in range(0, len(previous_failure), 8)]
-            state.test_function(TestCase.for_choices(choices))
-
-        if state.result is None:
-            state.run()
-
-        if state.result is None:
-            db.pop(test.__name__, None)
-        else:
-            db[test.__name__] = b''.join(i.to_bytes(8, 'big') for i in state.result)
-
-        if hasattr(db, 'close'):
-            db.close()
-
-        if state.result is not None:
-            test(TestCase.for_choices(state.result, print_results=True))
-    return accept
+    # Test case completed and was interesting
+    INTERESTING = 3
